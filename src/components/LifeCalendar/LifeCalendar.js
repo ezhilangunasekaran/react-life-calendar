a3592c8ea2f9b638b72169e1e3d4f90d2f350fd8
Ezhil <g.ezhilan1@gmail.com>
2017-11-13 22:26:03 -0500

    fixed data rendering issue in sidebar

--- src/components/LifeCalendar/LifeCalendar.js
+++ src/components/LifeCalendar/LifeCalendar.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 import { Grid } from '../Grid'
+import WeekList from '../WeekList'
+import SideBar from '../SideBar'
 
 class LifeCalendar extends Component {
+    state = { modelOpen: false, sideBarOpen: false, activeWeek: 0 }
     render(){
-        const { children } = this.props
+        const children = React.Children.map(this.props.children, child => {
+            if(child.type === WeekList){
+                return React.cloneElement(child,{
+                    onActiveClick : activeWeek => this.setState({ sideBarOpen: true, activeWeek })
+                })
+            }
+            if(child.type === SideBar){
+                return React.cloneElement(child,{
+                    sideBarOpen : this.state.sideBarOpen,
+                    activeWeek : this.state.activeWeek
+                })
+            }else{
+                return child
+            }
+        })
         return (
             <Grid gridTemplateRows= "40px auto" gridTemplateColumns='1fr auto' justifyContent="center">
                 {children}
